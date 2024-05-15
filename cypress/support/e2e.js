090a8b997685b01fe90af54446fba157280625b3
Wellington Brito <wellington1000@yahoo.com.br>
2024-05-15 19:14:52 -0300

    segundo commit

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,13 +15,11 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
-import '@shelex/cypress-allure-plugin';
 
 
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
-require('@shelex/cypress-allure-plugin');
 require('cypress-xpath');
 
 beforeEach(() => {
